b0a4301752afe4c08a489d733a6173e03ff28d36
harrykerry <haroldkerry@gmail.com>
2025-10-01 19:23:08 +0300

    fix(cors): enable CORS in backend
    - Enabled CORS to allow requests from frontend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200', // your Angular app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if you need cookies or Authorization headers
+  });
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
